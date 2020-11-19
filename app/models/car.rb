7b86c0bb872b0eaa8bb7f89c02d9ce50df7e2806
James Mernin <jamespmernin@gmail.com>
2020-11-18 19:08:07 -0500

    Validations added

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,7 @@
 class Car < ApplicationRecord
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true, :length {minimun:4, too_short: "The number you provided for year must be 4 characters"}
+  validates :moonroof, presence: true
+  validates :color, presence: true
 end
